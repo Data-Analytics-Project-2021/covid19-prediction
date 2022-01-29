6767aa301ba7aa28d8bac527da7365e3c3f345e6
Vibha Masti <vsmasti@gmail.com>
2022-01-29 13:32:14 +0000

    Fixed plotting

--- models/mult_timeseries/plotting.py
+++ models/mult_timeseries/plotting.py
@@ -2,6 +2,46 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
 
+def plot_dataframe(df, *, title, ylabel='', xlabel='Date', figpath=None, color='k'):
+    """
+    Plot daily cases/cumulative vaccinations
+    """
+    # Set the locator
+    locator = mdates.MonthLocator(interval=4)  # every 2 months
+    # Specify the format - %b gives us Jan, Feb...
+    fmt = mdates.DateFormatter('%b-%y')
+
+    plt.figure(figsize=(5,5), dpi=100)
+
+
+    plt.plot(df, color=color)
+
+    fig = plt.gcf()
+    ax = plt.gca()
+    plt.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+
+    ax.spines["top"].set_visible(True)
+    ax.spines["right"].set_visible(True)
+    ax.spines["left"].set_visible(True)
+    ax.spines["bottom"].set_visible(True)
+
+    # Locate months
+    ax.xaxis.set_major_locator(locator)
+
+    # Specify formatter
+    ax.xaxis.set_major_formatter(fmt)
+    
+    plt.xlabel=xlabel
+    plt.ylabel=ylabel
+
+    plt.title(title)
+    # plt.legend(loc='best', fontsize=8)
+    plt.show()
+    
+    
+    if figpath is not None:
+        fig.savefig(figpath, format='eps', bbox_inches='tight')
+
 
 def plot_train_test_fore(*, train, test, fore, conf=None, title='Forecast vs Actuals', ylabel='', xlabel='Date', figpath=None, start_date='2021-03-01'):
     """
@@ -27,8 +67,15 @@ def plot_train_test_fore(*, train, test, fore, conf=None, title='Forecast vs Act
     plt.plot(fore, label='forecast', color='r')
 
     fig = plt.gcf()
+    ax = plt.gca()
     x = plt.gca().xaxis
     plt.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+
+
+    ax.spines["top"].set_visible(True)
+    ax.spines["right"].set_visible(True)
+    ax.spines["left"].set_visible(True)
+    ax.spines["bottom"].set_visible(True)
 
     # Locate months
     x.set_major_locator(locator)
